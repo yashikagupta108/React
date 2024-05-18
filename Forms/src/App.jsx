c96e9745fba2376e03779674ba8804360cc0a53b
Shubham Vohra <shubham.vohra@geminisolution.com>
2024-05-18 20:10:04 +0530

    react practice

---
+++ Forms/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import "./../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <button class="btn btn-primary">click me</button>
+    </>
+  );
+}
+
+export default App;
