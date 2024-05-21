4d0cc5d3cc420f0576d92bb1c57df1c1fb6ad88c
yashikagupta108 <guptayashika108@gmail.com>
2024-05-21 14:20:40 +0530

    project setup commit

--- Forms/src/App.jsx
+++ Forms/src/App.jsx
@@ -5,11 +5,35 @@ import "./App.css";
 import "./../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [mob, setMob] = useState("");
+
+  mobCheck = () => {
+    this.setState;
+  };
 
   return (
     <>
-      <button class="btn btn-primary">click me</button>
+      <form>
+        <div class="pb-2">
+          <label class="label">First Name :</label>
+          <input type="text"></input>
+        </div>
+        <div class="pb-2">
+          <label class="label">Last Name :</label>
+          <input type="text"></input>
+        </div>
+        <div class="pb-2">
+          <label class="label" value={mob} onChange={mobCheck}>
+            Mobile Number :
+          </label>
+          <input type="tel"></input>
+        </div>
+        <div class="pb-2">
+          <label class="label">Email Id:</label>
+          <input type="text"></input>
+        </div>
+      </form>
+      <button class="btn btn-primary">Submit</button>
     </>
   );
 }
